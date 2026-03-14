847ffd6c5b3833fb622734b4636d47647172206f
edwardedo03 <edwardahhutauruk@gmail.com>
2026-03-14 13:28:55 +0700

    Add PHP

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
+<?php
+    include("connect_db.php");
+
+    function get_film_for_home($conn) {
+        $query = mysqli_query($conn, "SELECT * FROM top_10000_movies LIMIT 50");
+        
+        if(mysqli_num_rows($query) > 0) {
+            return $query;
+        } else {
+            return "Empty";
+        }        
+    }
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Film Holic</title>
+    <title>Movies Holic</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <nav id="nav">
         <div class="image-nav">
-            <a href="index.html"><img src="assets/image/logo-remove-bg.png" width="100px" alt="Film-Holic"></a>
+            <a href="index.php"><img src="assets/image/logo-remove-bg.png" width="150px" alt="Film-Holic"></a>
         </div>
 
         <div class="menu-nav">
@@ -33,7 +49,6 @@
                 <div class="marquee-items">
                     <img src="https://plus.unsplash.com/premium_photo-1673448391005-d65e815bd026?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHx8MA%3D%3D" alt="">
                     <img src="https://plus.unsplash.com/premium_photo-1673448391005-d65e815bd026?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHx8MA%3D%3D" alt="">
-                    <img src="https://plus.unsplash.com/premium_photo-1673448391005-d65e815bd026?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHx8MA%3D%3D" alt="">
                 </div>
             </marquee>
         </div>
@@ -42,30 +57,30 @@
     <section id="second-section">
         <h3>MOVIES</h3>
         <div class="movies-card-container">
+            <?php 
+                $result = get_film_for_home($conn);
+                
+                while ($row = mysqli_fetch_assoc($result)) {
+
+                    $genre_clean = $row['genre'];
+                    $items = array('[', ']', "'");
+                    $genre_clean = str_replace($items, "", $genre_clean);
+                    $genre_clean = str_replace(",", " •", $genre_clean);
+            ?>
+
             <div class="movies-card">
                 <a href="">
-                    <img src="https://plus.unsplash.com/premium_photo-1673448391005-d65e815bd026?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHx8MA%3D%3D" alt="" width="170px" height="220">
+                    <img src="<?php echo $row['image_url'] ?>" alt="film-image">
                 </a>
-                <a href=""><p>Sore</p></a>
-                <p>Horror, Psycopath</p>
-                <p>⭐ 5.0</p>
+                <div class="movie-info">
+                    <a href=""><p class="movie-title"><?php echo $row['title']?></p></a>
+                    <p><?php echo $genre_clean ?></p>
+                    <p>⭐ <?php echo $row['vote_average'] ?></p>
+                </div>
             </div>
-            <div class="movies-card">
-                <a href="">
-                    <img src="https://plus.unsplash.com/premium_photo-1673448391005-d65e815bd026?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHx8MA%3D%3D" alt="" width="170px" height="220">
-                </a>
-                <a href=""><p>Sore</p></a>
-                <p>Horror, Psycopath</p>
-                <p>⭐ 5.0</p>
-            </div>
-            <div class="movies-card">
-                <a href="">
-                    <img src="https://plus.unsplash.com/premium_photo-1673448391005-d65e815bd026?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90b3xlbnwwfHwwfHx8MA%3D%3D" alt="" width="170px" height="220">
-                </a>
-                <a href=""><p>Sore</p></a>
-                <p>Horror, Psycopath</p>
-                <p>⭐ 5.0</p>
-            </div>
+
+            <?php } ?>
+
         </div>
     </section>
 
